9ec086b86ffc24df969cda10b7881d174a970a60
Sainty93 <jenandfloyd93@icloud.com>
2022-11-14 22:57:21 -0800

    added js

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,75 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+const hour = new Date().getHours(); // 6, 10, 18
+
+hour-9
+hour-10
+hour-11
+hour-12
+hour-13
+hour-14
+hour-15
+hour-4
+hour-5
+hour-6
+hour-7
+
+
+var selectId;
+
+// marking current Date
+currentDateElement = document.querySelector(`#hour-${hour}`); // hour-12, hour-13
+currentDateElement.textContent = 'Current Day';
+
+// Current Day
+
+var pastStart;
+var pastEnd;
+var futureStart;
+var futureEnd;
+
+pastStart = 9;
+futureEnd = 19;
+
+pastEnd = hour - 1;
+futureStart = hour + 1;
+
+
+
+for (let i = pastStart; i <= pastEnd; i++) {
+  const selectId = `hour-{i}`;
+  const selectedElement = document.querySelector(`#{selectId}`);
+  selectedElement.classList.add("past");
+}
+
+for (let i = futureStart; i <= futureEnd; i++) {
+  const selectId = `hour-{i}`;
+  const selectedElement = document.querySelector(`#{selectId}`);
+  selectedElement.classList.add("past");
+}
+
+// Future
+for (let i = hour + 1; i < 23; i++) {
+  // same previous block
+  selectedElement.classList.add("future");
+  
+}
+
+
+
+let userInputs = {};
+userInputs = JSON.parse(localStorage.getItem('userInputs')); // undefined
+
+storedItem = localStorage.getItem('userInputs');
+userInputs = storedItem ? storedItem : {}; // shorthand if condition
+
+
+userInputs['20'] = 'some text here';
+
+localStorage.setItem('userInputs', JSON.stringify(userInputs));
+
+userInputs = JSON.parse(localStorage.getItem('userInputs'));
+
+// for ... in
+for (const hourDigit in userInputs) {
+  const targetElement = document.querySelector(`#hour-${hourDigit}`); // hour-14
+  targetElement.textContent = userInputs[hourDigit];
+}
